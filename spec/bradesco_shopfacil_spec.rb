811d5bfa569bae35ab9beab32f8a470d98afddc6
gilcierweb <gilcier06@yahoo.com.br>
2018-06-07 19:53:55 -0300

    adjusting test rspec

--- spec/bradesco_shopfacil_spec.rb
+++ spec/bradesco_shopfacil_spec.rb
@@ -3,7 +3,21 @@ RSpec.describe BradescoShopfacil do
     expect(BradescoShopfacil::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "sandbox default" do
+    merchant_id = '90000'
+    security_key = 'mykeysecret'
+    bradesco_shopfacil = BradescoShopfacil::Shopfacil.new(merchant_id, security_key)
+    sandbox = bradesco_shopfacil.sandbox
+
+    expect(sandbox).to eq(false)
+  end
+
+  it "sandbox true" do
+    merchant_id = '90000'
+    security_key = 'SUA CHAVE'
+    bradesco_shopfacil = BradescoShopfacil::Shopfacil.new(merchant_id, security_key)
+    sandbox = bradesco_shopfacil.sandbox = true
+
+    expect(sandbox).to eq(true)
   end
 end
